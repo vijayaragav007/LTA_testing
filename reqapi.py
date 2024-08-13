c706871b18266ca7f72de3bcf0b8da892fd94d6d
vijayaragav007 <vijaymathan002@gmail.com>
2024-08-13 12:54:14 +0530

    file modified

---
+++ reqapi.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
+
+print(response.status_code)
+print(response.json())
+
+assert response.status_code == 200
